73ac59396f279f9686aa98d598ae022a1c386f27
Tan Ngo <tan_ngo@datahouse.com>
2025-10-04 16:32:59 +0700

    feat(bv): test create image 2

--- src/modules/roles/controllers/createRole/createRole.endpoint.ts
+++ src/modules/roles/controllers/createRole/createRole.endpoint.ts
@@ -9,6 +9,7 @@ import { ApiTags } from '@nestjs/swagger';
 @ApiTags('Roles')
 @UseInterceptors(ResponseInterceptor)
 export class UsersEndpoint {
+  //test image
   constructor(private readonly roleService: RoleService) {}
 
   @Post()
